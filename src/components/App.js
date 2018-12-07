ef2755841f738acc16f6a3b355acd22907fdc7e0
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-12-07 19:03:35 +0000

    feat: create News and NewSingle component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import News from './News/News'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          
+          <h1>News Feed</h1>
         </header>
+
+        <News />
       </div>
     );
   }
---
+++ src/components/News/NewSingle.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NewSingle extends Component {
+
+  render() {
+    return (
+      <h4>News Item</h4>
+    )
+  }
+}
+
+export default NewSingle;
