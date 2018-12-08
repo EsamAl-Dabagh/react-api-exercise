eed0ea8c4d22d1e9b235bc8c9c0d346c7402451b
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-12-08 09:54:45 +0000

    feat: componentDidMount() GETS CBC-News articles from NewsAPI using fetch

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react';
 import NewSingle from './NewSingle';
 
 class News extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      news: []
+    }
+  }
+
+  componentDidMount() {
+    const url = `https://newsapi.org/v2/top-headlines?sources=cbc-news&apiKey=${process.env.REACT_APP_API_NEWSAPI_KEY}`;
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          news: data.articles
+        })
+      })
+      .catch((error) => console.log(error))
+  }
+
   renderItems() {
-    return this.props.items.map( (item) => (
-      <NewSingle key={item.id} />
+    return this.state.news.map( (item) => (
+      <NewSingle key={item.id} item={item} />
     ));
   }
 
